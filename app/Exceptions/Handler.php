3c16956383f903ae2a28fdcbd48ec35cc4d88e43
Yuri Corona Lopes <yuriclopes@gmail.com>
2018-01-27 15:03:44 -0200

    redirect non existing route to root

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,23 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($this->isHttpException($exception))
+        {
+            switch ($exception->getStatusCode()) 
+                {
+                // not found
+                case 404:
+                    return redirect()->guest('/');
+                    break;
+
+                default:
+                    return $this->renderHttpException($exception);
+                break;
+            }
+        }
+        else
+        {
+                return parent::render($request, $exception);
+        }
     }
 }

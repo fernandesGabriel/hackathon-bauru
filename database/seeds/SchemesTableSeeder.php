be40dd50ce8036e40a4a7f991760d0b60dd71cee
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2018-01-27 19:16:15 -0200

    Fix schemes seeder

--- database/seeds/SchemesTableSeeder.php
+++ database/seeds/SchemesTableSeeder.php
@@ -14,11 +14,11 @@ class SchemesTableSeeder extends Seeder
     {
         $schemes = [
         	['Homeapge', json_encode(['page_title' => '']), 'homepage'],
-        	['Single Content', json_encode(['page_title' => '', 'title_1' => '', 'content_1']), 'single-content'],
+        	['Single Content', json_encode(['page_title' => '', 'title_1' => '', 'content_1' => '']), 'single-content'],
         	['Single Content Call to Action', json_encode(['page_title' => '', 'title_1' => '', 'content_1']), 'single-content-call-action'],
-        	['Five Content with Sidebar', json_encode(['page_title' => '', 'title_1' => '', 'content_1', 'title_2' => '', 'content_2', 'title_3' => '', 'content_3', 'title_4' => '', 'content_4', 'title_5' => '', 'content_5']), 'five-contents-sidebar'],
-        	['Donation', json_encode(['page_title' => '', 'title_1' => '', 'content_1']), 'donation'],
-        	['Contact', json_encode(['page_title' => '', 'title_1' => '', 'content_1']), 'contact'],
+        	['Five Content with Sidebar', json_encode(['page_title' => '', 'title_1' => '', 'content_1' => '', 'title_2' => '', 'content_2' => '', 'title_3' => '', 'content_3' => '', 'title_4' => '', 'content_4' => '', 'title_5' => '', 'content_5' => '']), 'five-contents-sidebar'],
+        	['Donation', json_encode(['page_title' => '', 'title_1' => '', 'content_1' => '']), 'donation'],
+        	['Contact', json_encode(['page_title' => '', 'title_1' => '', 'content_1' => '']), 'contact'],
         ];
 
         foreach ($schemes as $key => $row) {

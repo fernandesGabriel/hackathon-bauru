f8b74b5012d00a0050d19e399db8c05786f6d420
Yuri Corona Lopes <yuriclopes@gmail.com>
2018-01-27 13:32:53 -0200

    models

---
+++ app/Models/Gallery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hackathon\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Gallery extends Model
+{
+    use SoftDeletes;
+    /**
+    * The database table used by the model.
+    *
+    * @var string
+    */
+    protected $table = 'gallery';
+    
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = [ 'name', 'description', 'page_id' ];
+}

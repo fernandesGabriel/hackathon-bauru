91a7b835b222815c351f16793f5f6dd073860f79
Gilberto Marcomini <gilberto.marcomini@gruponewway.com.br>
2018-01-27 14:15:47 +0000

    Migrations

---
+++ database/migrations/2018_01_27_134945_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->string('path', 255);
+            $table->text('description')->nullable();
+            $table->text('data')->nullable();
+            $table->integer('menu_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
---
+++ database/migrations/2018_01_27_135019_create_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('scheme_id');
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->string('url', 255);
+            $table->text('content')->nullable();
+            $table->text('keywords')->nullable();
+            $table->integer('menu_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}

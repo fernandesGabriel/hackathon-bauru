ba5d7a32e4240f938f127d76c7284cec66bd3fbd
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2018-01-28 11:39:23 -0200

    Fix settings model

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Settings extends Model
 {
+    public $timestamps = false;
+
     protected $primaryKey = 'key';
 
     /**
@@ -14,4 +16,11 @@ class Settings extends Model
      * @var string
      */
     protected $table = 'settings';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [ 'key', 'value' ];
 }
